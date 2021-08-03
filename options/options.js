083234e81f7ba45ca92256823082cfc6540786c0
Crowdbotics <team@crowdbotics.com>
2021-08-03 03:31:34 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "curly_cell_29369Identifier",
+  url: "https://curly_cell_29369Identifier.botics.co",
+  api: "https://curly_cell_29369Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
